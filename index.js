c06086e3be951ccc9deea69f5ab652cbc64048fc
Eric Cornelissen <ericornelissen@gmail.com>
2019-03-13 18:55:53 +0200

    Refactor git module
    Remove unused code (namely `getStatusByMatcher`), use more modern 
    JavaScript (e.g. `const` and `let`), remove redundant function 
    parameters, and improve naming.

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 var map = require('map-stream');
 var git = require('./lib/git');
 
-var gitApp = 'git';
 var gitExtra = {env: process.env};
 
 var scssLintPlugin = function() {
   return map(function(file, cb) {
-    git.which(gitApp, function(err) {
+    git.which(function(err) {
       if (err) {
         return cb(new Error('git not found on your system.'));
       }
 
-      git.exec(gitApp, ['add', file.path], gitExtra, function(err, stdout) {
+      git.exec(['add', file.path], gitExtra, function(err, stdout) {
         if (err) {
           return cb(new Error('git add failed.'));
         }
