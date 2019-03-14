614c5ddf1ac39a7b9302dfcd19b55861e4d4689f
Eric Cornelissen <ericornelissen@gmail.com>
2019-03-14 14:43:12 +0200

    Add support for plugin options (#28)
    * refactor: enable configuration of plugin
    
    * docs: git execute function
    
    * docs: add configuration object to README

--- src/git.js
+++ src/git.js
@@ -5,9 +5,26 @@ const which = require("which");
 const git = "git";
 const add = "add";
 
+const defaultOptions = { env: process.env };
 const limiter = new Bottleneck({ maxConcurrent: 1 });
 
-function gitExecute(args, options, callback) {
+/**
+ * Execute arbitrary git commands one at the time. When
+ * multiple calls are performed in quick succession the
+ * git commands are automatically rate limited.
+ *
+ * Example:
+ * ```
+ * gitExecute(['add', 'myfile.txt'], options, callback);
+ * // -> executes: $ git add myfile.txt
+ * ```
+ *
+ * @param  {Array}    args     The arguments for the command in order.
+ * @param  {Object}   _options Options for the command.
+ * @param  {Function} callback Function called on completion.
+ */
+function gitExecute(args, _options, callback) {
+  let options = Object.assign(defaultOptions, _options);
   return limiter.submit(exec, git, args, options, callback);
 }
 
@@ -27,8 +44,8 @@ Object.defineProperty(_export, "available", {
   },
 });
 
-_export.stage = function(file, options, callback) {
-  return gitExecute([add, file], options, callback);
+_export.stage = function(file, config, callback) {
+  return gitExecute([add, file], config, callback);
 };
 
 module.exports = _export;
