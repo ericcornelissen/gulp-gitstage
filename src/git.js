e0defa418837ff9b68bb76a9b428c9f64fdfef7d
Eric Cornelissen <ericornelissen@gmail.com>
2019-03-14 11:39:02 +0200

    docs: git execute function

--- src/git.js
+++ src/git.js
@@ -8,6 +8,21 @@ const add = "add";
 const defaultOptions = { env: process.env };
 const limiter = new Bottleneck({ maxConcurrent: 1 });
 
+/**
+ * Execute arbitrary git commands one at the time. When
+ * multiple calls are performed in quick succession the
+ * git commands are automatically rate limited.
+ *
+ * Example:
+ * ```
+ * gitExecute(['add', 'myfile.txt'], options, callback);
+ * // -> executes: $ git add myfile.txt
+ * ```
+ *
+ * @param  {Array}    args     The arguments for the command in order.
+ * @param  {Object}   _options Options for the command.
+ * @param  {Function} callback Function called on completion.
+ */
 function gitExecute(args, _options, callback) {
   let options = Object.assign(defaultOptions, _options);
   return limiter.submit(exec, git, args, options, callback);
