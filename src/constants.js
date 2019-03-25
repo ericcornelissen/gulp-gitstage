fefc4e0c2912a6a49dcdb460d62858b16c05644c
Eric Cornelissen <ericornelissen@gmail.com>
2019-03-25 22:35:01 +0200

    docs: inline documentation for constants module

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,34 @@
+/**
+ * Provides constants used in one or more modules in the project.
+ * @module constants
+ */
 module.exports = {
+  /**
+   * Tag that should be used for errors thrown by the package.
+   * @constant {String}
+   */
   errorTag: "gulp-gitstage",
 
-  // git commands\options
-  add: "add",
+  /* === git commands\options === */
+
+  /**
+   * String denoting the 'git' command.
+   * @constant {String}
+   */
   git: "git",
+
+  /**
+   * String denoting the 'add' verb for 'git'. See {@link
+   * https://git-scm.com/docs/git-add the docs}.
+   * @constant {String}
+   */
+  add: "add",
+
+  /**
+   * String denoting the 'update' option for the 'add' verb for 'git'. See
+   * {@link https://git-scm.com/docs/git-add#Documentation/git-add.txt--u
+   * the docs}.
+   * @constant {String}
+   */
   update: "-u",
 };
