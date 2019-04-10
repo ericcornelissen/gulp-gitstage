93e5b87e16b7f866067bd5744e5e65cc0d9ea4cf
Eric Cornelissen <ericornelissen@gmail.com>
2019-04-10 23:02:53 +0300

    docs: improve existing documentation
    Overall fixes:
    - Improve readability by adding a "-" after parameter names
    - Remove indentation to line up documentation as it may clutter the diff 
    in the future (where lines are changed for the sole purpose of 
    indentation).
    
    Other fixes:
    - Use intended function name as value for @exports in index.js
    - Fixed type: otherwse --> otherwise

--- src/index.js
+++ src/index.js
@@ -8,13 +8,13 @@ const git = require("./git.js");
 /**
  * Exposes the plugin 'gitstage' to be used in {@link https://gulpjs.com/ Gulp}.
  * The plugin will {@link https://git-scm.com/docs/git-add stage} all files in
- * the stream (unless configured otherwse) to a {@link https://git-scm.com/ git}
- * repository.
+ * the stream (unless configured otherwise) to a {@link https://git-scm.com/
+ * git} repository.
  *
- * @exports gulp-gitstage
- * @param  {String} [gitCwd=cwd]        Override from which directory git is executed.
- * @param  {Boolean} [stagedOnly=false] Only stage previously staged files.
- * @return {Stream}                     Identity file stream, does not modify the input.
+ * @exports gitstage
+ * @param  {String} [gitCwd=cwd] - Override from which directory git is executed.
+ * @param  {Boolean} [stagedOnly=false] - Only stage previously staged files.
+ * @return {Stream} Identity file stream, does not modify the input.
  */
 module.exports = function(config = {}) {
   const streamId = Math.random();
