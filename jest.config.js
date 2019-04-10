eef413710b3d14f48e3f43632c407900a3ad93d4
Eric Cornelissen <ericornelissen@gmail.com>
2019-04-10 18:58:56 +0300

    chore: add coverage thresholds in Jest
    The coverage thresholds are set in Jest rather than Codecov in order for 
    contributors to fully reproduce the build failure locally, rather than 
    having to go to the Codecov website.

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,12 @@
 module.exports = {
   coverageDirectory: "./_reports/coverage",
+  coverageThreshold: {
+    global: {
+      branches: 95,
+      functions: 100,
+      lines: 95,
+    },
+  },
   collectCoverageFrom: ["./src/**/*.js"],
 
   setupFilesAfterEnv: ["./test/setup.js"],
