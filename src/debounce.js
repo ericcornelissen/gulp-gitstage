8ec7182c33a47cf73a44b25437b07b194e4b6582
Eric Cornelissen <ericornelissen@gmail.com>
2019-04-11 15:20:15 +0300

    Hide debounce dependency into its own module (#56)

---
+++ src/debounce.js
@@ -0,0 +1,25 @@
+const { types } = require("./constants.js");
+
+/**
+ * Provides a function to debounce calls to a specific function.
+ * @module debounce
+ * @param  {Function} fn - the function to debounce.
+ * @param  {Number} timeout - the time (in milliseconds) to wait before executing {@link fn}.
+ * @return {Function} The debounced function.
+ * @example
+ * const debouncedLog = debounce(console.log, 100);
+ * debouncedLog("foo");
+ * debouncedLog("bar");
+ * // -> "bar"
+ */
+module.exports = function(fn, timeout) {
+  const debounce = require("debounce");
+
+  if (typeof fn !== types.function) {
+    throw new TypeError("fn is not a function");
+  } else if (typeof timeout !== types.number) {
+    throw new TypeError("timeout is not a number");
+  } else {
+    return debounce(fn, timeout, false);
+  }
+};
